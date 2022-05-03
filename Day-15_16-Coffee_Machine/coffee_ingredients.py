10749f8c7d9b12812f1557b9c5f664fc0df76650
Angela Montez <91339804+almontez@users.noreply.github.com>
2022-05-03 11:50:51 -0700

    Add files via upload

---
+++ Day-15_16-Coffee_Machine/coffee_ingredients.py
@@ -0,0 +1,27 @@
+MENU = {
+    "a": {
+        "drink": "Espresso",
+        "display": "A: Espresso - $1.50",
+        "ingredients": {"water": 50, "coffee": 18, },
+        "cost": 1.50,
+    },
+    "b": {
+        "drink": "Latte",
+        "display": "B: Latte - $2.50",
+        "ingredients": {"water": 200, "milk": 150, "coffee": 24, },
+        "cost": 2.50,
+    },
+    "c": {
+        "drink": "Cappuccino",
+        "display": "C: Cappuccino - $3.00",
+        "ingredients": {"water": 250, "milk": 100, "coffee": 24, },
+        "cost": 3.00,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
